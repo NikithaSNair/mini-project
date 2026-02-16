f7b94b6f3a4c302a0bba5a19761ed478597a8174
Anjumie <anjum.aysha.ar@gmail.com>
2026-02-16 14:24:17 +0530

    Added heuristic detection logic

---
+++ testHeuristic.js
@@ -0,0 +1,29 @@
+const { calculateRisk, getRiskLevel } = require('./heuristic');
+
+const testUrls = [
+
+    "https://google.com",
+
+    "http://192.168.1.1/login",
+
+    "https://paypal-secure-login.com",
+
+    "https://example.com",
+
+    "http://verify-account-bank.com"
+
+];
+
+testUrls.forEach(url => {
+
+    const score = calculateRisk(url);
+
+    const level = getRiskLevel(score);
+
+    console.log("\nURL:", url);
+
+    console.log("Score:", score);
+
+    console.log("Risk Level:", level);
+
+});
